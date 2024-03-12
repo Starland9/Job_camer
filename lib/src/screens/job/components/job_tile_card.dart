25ab30439ea69c4510212ad514d0a8d0a0bf912a
Landry Simo <landrysimo99@gmail.com>
2024-03-12 18:12:09 +0100

    Commit Description: - Added Pod configuration file for debug mode in Runner project - Added Pods-Runner release configuration file to project settings - Updated project with new Pods and frameworks - Added reference to Pods.xcodeproj in code - Refactored JobTileCard class to use StatefulWidget and added onTap event - Updated card design for JobTileCard for better user experience - Refactored UI layout for Job Details screen with improved styling and functionality

--- lib/src/screens/job/components/job_tile_card.dart
+++ lib/src/screens/job/components/job_tile_card.dart
@@ -1,72 +1,87 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:job_camer/src/screens/job/job_screen.dart';
 
-class JobTileCard extends StatelessWidget {
+class JobTileCard extends StatefulWidget {
   const JobTileCard({
     super.key,
   });
 
   @override
+  State<JobTileCard> createState() => _JobTileCardState();
+}
+
+class _JobTileCardState extends State<JobTileCard> {
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      surfaceTintColor: Colors.white70,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              const Expanded(
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: CachedNetworkImageProvider(
-                      "https://picsum.photos/200",
-                    ),
-                  ),
-                  title: Text(
-                    "Job Title",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text("Company Name"),
-                ),
-              ),
-              IconButton(
-                onPressed: _more,
-                icon: const Icon(Icons.more_vert),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return GestureDetector(
+      onTap: _goToJobDetails,
+      child: Card(
+        elevation: 1,
+        surfaceTintColor: Colors.white70,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
               children: [
-                const Text('Il y a 10 jours'),
-                Text.rich(
-                  TextSpan(
-                    text: "150K XAF",
-                    style: TextStyle(
-                      color: Get.theme.primaryColor,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    children: const [
-                      TextSpan(
-                        text: "/Mo",
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
+                const Expanded(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: CachedNetworkImageProvider(
+                        "https://picsum.photos/200",
                       ),
-                    ],
+                    ),
+                    title: Text(
+                      "Job Title",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text("Company Name"),
                   ),
-                )
+                ),
+                IconButton(
+                  onPressed: _more,
+                  icon: const Icon(Icons.more_vert),
+                ),
               ],
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Il y a 10 jours'),
+                  Text.rich(
+                    TextSpan(
+                      text: "150K XAF",
+                      style: TextStyle(
+                        color: Get.theme.primaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      children: const [
+                        TextSpan(
+                          text: "/Mo",
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 
   void _more() {}
+
+  void _goToJobDetails() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return const JobScreen();
+    }));
+  }
 }
--- lib/src/screens/job/job_screen.dart
+++ lib/src/screens/job/job_screen.dart
@@ -1,10 +1,76 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class JobScreen extends StatelessWidget {
   const JobScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(15),
+        child: SafeArea(
+          child: Column(
+            children: [
+              const Row(
+                children: [BackButton()],
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // BackButton(),
+                  Expanded(
+                    child: CircleAvatar(
+                      backgroundImage: CachedNetworkImageProvider(
+                        "https://picsum.photos/200",
+                      ),
+                      radius: 60,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Job Title',
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Get.theme.primaryColor,
+                            ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: Get.height * 0.02),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("🏠 Company"),
+                      SizedBox(width: 5),
+                      Text("📅 Location"),
+                      SizedBox(width: 5),
+                      Text("⏳ Time"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: _showDescription,
+                        child: const Text("Description"),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  void _showDescription() {}
 }
